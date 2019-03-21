e494f288e4d3f1678d16207fb290723f815f9e11
Edufacr <edumadrim@gmail.com>
2019-03-20 21:54:12 -0600

    Se agregan las pruebas

--- main.cpp
+++ main.cpp
@@ -4,5 +4,22 @@
 int main() {
     RecipeList* List = new RecipeList();
     Menu* menu = new Menu(List);
+    List->InsertFirst(new Recipe("Uno",1));
+    List->InsertFirst(new Recipe("Dos",2));
+    List->InsertFirst(new Recipe("Tres",3));
+    List->InsertFirst(new Recipe("Cuatro",4));
+    List->InsertFirst(new Recipe("Cinco",5));
+    RecipeNode* tmp = List->FirstNode;
+    while(tmp != nullptr){
+        tmp->recipe->AddIngredient(1,"Banano","kg");
+        tmp->recipe->AddIngredient(1,"Mango","hg");
+        tmp->recipe->AddIngredient(1,"Kiwi","g");
+        tmp->recipe->AddIngredient(4,"Avena","ml");
+        tmp->recipe->AddIngredient(2,"Leche","l");
+        tmp->recipe->AddIngredient(50,"Helado","Bolitas");
+        tmp = tmp->next;
+    }
+
     menu->MainMenu();
+
 }
